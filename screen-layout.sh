542dcde1f09361cb68c303637334ef351afb2358
Xdaco <xdaco.xinu@gmail.com>
2021-09-23 21:36:08 +0200

    Initial i3 config

---
+++ screen-layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off --output VGA-1 --off --output DVI-I-4-4 --off --output DVI-I-3-3 --off --output DVI-I-2-2 --off --output DVI-I-1-1 --mode 2560x1440 --pos 1920x0 --rotate normal
